7092bd1aef58c308cb7f09c42da9bc683266c7e3
Taufik <ataufik135@gmail.com>
2023-01-16 09:13:50 +0700

    Update run_in_loop_headless.sh

--- run_in_loop_headless.sh
+++ run_in_loop_headless.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-cd /opt/bot
-source /opt/bot/env/bin/activate
-wait_time=5
+cd /robot
+source /robot/env/bin/activate
+wait_time=20
 
 if [[ $# -eq 1 ]]; then
-    browser_count=$1
+  browser_count=$1
 elif [[ $# -eq 2 ]]; then
-    browser_count=$1
-    wait_time=$2
+  browser_count=$1
+  wait_time=$2
 fi
 
-while true;
-do
-    if [[ $browser_count -gt 1 ]]; then
-        echo "Running with ${browser_count} browsers..."
-        python run_ad_clicker.py -qf q.txt -pf p.txt --headless -bc ${browser_count}
-    else
-        python run_ad_clicker.py -qf q.txt -pf p.txt --headless
-    fi
+while true; do
+  if [[ $browser_count -gt 1 ]]; then
+    echo "Running with ${browser_count} browsers..."
+    python3.9 run_ad_clicker.py -qf q.txt -pf p.txt --headless -bc ${browser_count}
+  else
+    python3.9 run_ad_clicker.py -qf q.txt -pf p.txt --headless -bc 1
+  fi
 
-    echo "Sleeping ${wait_time} seconds..."
-    sleep $wait_time
+  echo "Sleeping ${wait_time} seconds..."
+
+  filename='adclicker.log'
+  touch $filename
+  if [ -f $filename ]; then
+    rm adclicker.log
+    echo "$filename is removed"
+  fi
+
+  sleep $wait_time
 done
